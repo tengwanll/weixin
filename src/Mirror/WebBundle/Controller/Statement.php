a301215a887a47098499e1486b2ab00fc065ace0
mac <wangjiyu@9086.cn>
2017-11-15 13:02:36 +0800

    添加报表路由

---
+++ src/Mirror/WebBundle/Controller/Statement.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 31726
+ * Date: 2017/11/3
+ * Time: 10:31
+ */
+
+namespace Mirror\WebBundle\Controller;
+
+
+use Mirror\ApiBundle\Util\Helper;
+use Mirror\ApiBundle\Util\WeixinHelper;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/statement")
+ * Class IndexController
+ * @package Mirror\WebBundle\Controller
+ */
+class StatementController extends Controller
+{
+    /**
+     * @Route()
+     * @Template()
+     * @param Request $request
+     * @return array|\Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request){
+        $code=$request->get('code','');
+        $openId = $this->getRequest ()->getSession ()->get ( 'openId', '' );
+        if (!$openId) {
+            $result = WeixinHelper::getWeixinId ( $code );
+            $openId=Helper::getc($result,'openid','');
+            $this->getRequest ()->getSession ()->set ( 'openId', $openId );
+        }
+        $status=$this->get('user_service')->checkLogin($openId);
+        if(!$status){
+            return $this->render('MirrorWebBundle:Login:index.html.twig',array('openId'=>$openId));
+        }
+        return array('openId'=>$openId);
+    }
+}
