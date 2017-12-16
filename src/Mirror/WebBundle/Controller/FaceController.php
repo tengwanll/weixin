9840eae13e51e6d8ea9485fbd2b57bfa8df7fe40
zhengyue <zhengyue@minshengwuyou.com>
2017-12-16 16:51:38 +0800

    美容

--- src/Mirror/WebBundle/Controller/FaceController.php
+++ src/Mirror/WebBundle/Controller/FaceController.php
@@ -11,7 +11,8 @@ namespace Mirror\WebBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpKernel\Tests\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 
 /**
  * @Route("/face")
