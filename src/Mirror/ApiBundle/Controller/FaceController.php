8c02ed80c170d62523f0921803b1e8d71c6d1bad
zhengyue <zhengyue@minshengwuyou.com>
2017-12-18 23:02:37 +0800

    美容

--- src/Mirror/ApiBundle/Controller/FaceController.php
+++ src/Mirror/ApiBundle/Controller/FaceController.php
@@ -9,6 +9,7 @@
 namespace Mirror\ApiBundle\Controller;
 
 
+use Mirror\ApiBundle\Entity\BoxInfo;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -38,7 +39,15 @@ class FaceController extends BaseController
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
     public function update(Request $request){
-        $boxInfo=$this->serializerByJson($request,'boxInfo');
+        $json=$this->getJson($request);
+        $boxInfo=new BoxInfo();
+        $boxInfo->setAbility(json_encode($json->get('ability',[])));
+        $boxInfo->setName($json->get('name',''));
+        $boxInfo->setAge($json->get('age',0));
+        $boxInfo->setGender($json->get('gender',0));
+        $boxInfo->setBoxId($json->get('boxId',''));
+        $boxInfo->setEmail($json->get('email',''));
+        $boxInfo->setTelephone($json->get('telephone',''));
         $rr=$this->get('face_service')->update($boxInfo);
         return $this->buildResponse($rr);
     }
