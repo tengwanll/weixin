dd10e4e323fd0c7bc96fe8cb6e73298b9f44fdb5
zhengyue <zhengyue@minshengwuyou.com>
2017-12-18 21:57:02 +0800

    美容

--- src/Mirror/WebBundle/Resources/public/js/face.js
+++ src/Mirror/WebBundle/Resources/public/js/face.js
@@ -4,4 +4,30 @@ $(document).ready(function () {
         now.getFullYear() + '-' + now.getMonth()+ '-' + now.getDate() + ' '  +
         now.getHours() + ':' + now.getMinutes() + ':' +now.getSeconds()
     );
+    $('input[type="checkbox"]').bootstrapSwitch({
+        size: 'mini',
+        onText: '',
+        offText: '',
+        handleWidth: 10
+    });
+
+    $('#info-form').on('submit', e => {
+        e.preventDefault();
+
+    var result = $('#info-form').serializeArray().reduce(function(obj, item) {
+        if (item.name === 'ability') {
+            obj[item.name] = obj[item.name] || [];
+            obj[item.name].push(item.value);
+            return obj;
+        }
+        obj[item.name] = item.value;
+        return obj;
+    }, {});
+    result.ability = result.ability || [];
+
+    console.log(result);
+
+    // send request
+    // todo
+    })
 })
