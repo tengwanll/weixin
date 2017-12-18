53f9af991e7e8dd6d89a39a07eabd31f7a2cd0ad
zhengyue <zhengyue@minshengwuyou.com>
2017-12-18 16:44:54 +0800

    美容

--- src/Mirror/WebBundle/Resources/public/js/face.js
+++ src/Mirror/WebBundle/Resources/public/js/face.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     const now = new Date();
     $('#now').text(
         now.getFullYear() + '-' + now.getMonth()+ '-' + now.getDate() + ' '  +
-        now.getHours() + ':' + now.getMinutes() + ':' +now.getSeconds() 
+        now.getHours() + ':' + now.getMinutes() + ':' +now.getSeconds()
     );
-    
 })
