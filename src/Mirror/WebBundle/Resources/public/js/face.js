2228df44e6fe408a63b21baa4f98bddcbcec7a64
zhengyue <zhengyue@minshengwuyou.com>
2017-12-18 22:45:08 +0800

    美容

--- src/Mirror/WebBundle/Resources/public/js/face.js
+++ src/Mirror/WebBundle/Resources/public/js/face.js
@@ -11,26 +11,5 @@ $(document).ready(function () {
         handleWidth: 10
     });
 
-    $('#info-form').on('submit', function(event) {
-        event.preventDefault();
-
-        var result = $('#info-form').serializeArray().reduce(function(obj, item) {
-            if (item.name === 'ability') {
-                obj[item.name] = obj[item.name] || [];
-                obj[item.name].push(item.value);
-                return obj;
-            }
-            obj[item.name] = item.value;
-            return obj;
-        }, {});
-        result.ability = result.ability || [];
-        result.boxId=boxId;
-
-        ajaxAction("post",'/api/face',result,false,function(data,textStatus){
-            $('#page1').hide();
-            $('#page2').show();
-        },function(errno,errmsg){
-
-        });
-    })
+    
 })
