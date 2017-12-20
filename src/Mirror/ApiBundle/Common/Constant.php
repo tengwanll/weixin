4e1da00c1fedce22c29795e9ecb39ebf7b5284a7
zhengyue <zhengyue@minshengwuyou.com>
2017-12-20 10:09:51 +0800

    美容

--- src/Mirror/ApiBundle/Common/Constant.php
+++ src/Mirror/ApiBundle/Common/Constant.php
@@ -34,8 +34,8 @@ class Constant {
     public static $secret = '1ebfe6bec0c027b9a97750d183b56ce9';
     public static $WX_NOTIFY_PATH='weixin.amogene.com/api/order/notify';
     public static $accessTokenUrl = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s';
-    public static $userInfoUrl='https://api.weixin.qq.com/sns/user/info?access_token=%s&openid=%s';
-
+    public static $userInfoUrl='https://api.weixin.qq.com/cgi-bin/user/info?access_token=%s&openid=%s';
+    
     //盒子
     public static $box_status_start=1;//盒子初始生成
     public static $box_status_filled=2;//用户已经填写
